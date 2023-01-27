e235dd0bba96fbfee36fa8315fe73dcb7fd410c3
Nick <so2niko@gmail.com>
2023-01-27 15:27:50 +0200

    finish shop

--- shop/main.js
+++ shop/main.js
@@ -1,13 +1,27 @@
 const DB_URL =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQYwBebYrZ3-YdcNbnOJI7-YkfhlWOrIrM51StMQtX4Zv_XuxkQXTVC2rvLepPBqUQyUTqR775xZa1Y/pub?gid=0&single=true&output=tsv";
+const BOT_API_KEY = '5911496594:AAF3YEsQVLMRTWc8YwZ0JHG-kVGZvEZjx14';
 const BOOKS_CONTAINER_SELECTOR = ".books";
+const ORDER_BUTTON_SELECTOR = "#btn-order";
+const ORDER_INPUT_SELECTOR = "#input-order-tel";
 const BOOKS_DATA = [];
+let CHOSEN_BOOK = null;
 
 const onClickDetails = ({ target }) => {
     const isBtnText = target.dataset.is;
     if(isBtnText && isBtnText == 'show-modal-btn'){
         renderModalWindow(target.dataset.id);
     }
+}
+
+const onClickOrder = () => {
+  const input = document.querySelector(ORDER_INPUT_SELECTOR);
+  const tel = input.value;
+
+  console.log('ORDER: ');
+  console.log(`PHONE: ${tel}`);
+  console.log(`BOOK: ${JSON.stringify(CHOSEN_BOOK)}`);
+
 }
 
 const getBooksHTML = ({ Name, Description, Image, Id }) => {
@@ -41,7 +55,8 @@ const renderModalWindow = (id) => {
     const body = document.querySelector('.modal-body');
     //render
     //знайти книжку по id
-    const {Name, Author, Publisher, PubYear, Language, Price, Format, Cover, Description, Image} = BOOKS_DATA.find(({Id}) => Id == id);
+    CHOSEN_BOOK = BOOKS_DATA.find(({Id}) => Id == id);
+    const {Name, Author, Publisher, PubYear, Language, Price, Format, Cover, Description, Image} = CHOSEN_BOOK;
     //зарендерити її
     title.innerText = Name;
 
@@ -52,7 +67,9 @@ const renderModalWindow = (id) => {
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title text-warning bg-dark p-3">${ Price } UAH</h5>
+          <h5 class="card-title text-warning bg-dark p-3">${ Price } UAH <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#order-container" aria-expanded="false" aria-controls="order-container">
+          Замовити
+        </button></h5>
           <p class="card-text">${ Author }</p>
           <p class="card-text"> ${ Publisher } : ${ PubYear }</p>
           <p class="card-text text-muted">${ Language }, ${ Format }, ${ Cover }</p>
@@ -89,9 +106,13 @@ const load = async () => {
   BOOKS_DATA.push(...data);
   renderBooks(data);
   document.querySelector(BOOKS_CONTAINER_SELECTOR).addEventListener('click', onClickDetails);
+  document.querySelector(ORDER_BUTTON_SELECTOR).addEventListener('click', onClickOrder);
 
 
   console.log(data);
 };
 
 load();
+
+const getUpd = `https://api.telegram.org/bot${BOT_API_KEY}/getUpdates`;
+fetch(getUpd).then(r => r.json()).then(console.log);
